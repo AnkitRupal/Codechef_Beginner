accfcd3de1074665940a7ca26397694ec7242b3d
Ankit Rupal <75999622+AnkitRupal@users.noreply.github.com>
2021-02-28 15:14:42 +0530

    Update Minimum and Maximum.cpp

--- Minimum and Maximum.cpp
+++ Minimum and Maximum.cpp
@@ -1,35 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define ll long long int
-
-void input_array(int a[],int size){
-	for(int i=0;i<size;i++){
+void input_array(long long int a[],long long int size){
+	for(long long int i=0;i<size;i++){
 		cin>>a[i];
 	}
 }
 
-int min_index(int x,int y,int count,int i){
-	if(x>y){
-		return i;
-	}else{
-		return count;
-	}
-}
-
 int main(){
-	int t,n;
+	long long int t,n;
 	cin>>t;
 	
 	while(t--){
 		cin>>n;
-		int a[n],count=0;
-		unsigned long long int cost=0;
+		long long int a[n];
 		input_array(a,n);
-		for(int i=1;i<n;i++){
-			count=min_index(a[count],a[i],count,i);
-			cost+=a[count];
-		}
+		long long int cost=0;
+		sort(a,a+n);
+		cost=a[0]*(n-1);
 		cout<<cost<<endl;
 	}
 	
